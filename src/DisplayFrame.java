4fd5480cf47c1ca40c9183b527ca4cea697cd7ef
thomas1242 <thomaspalmer121@gmail.com>
2017-07-11 00:56:07 -0400

    refactor

--- src/DisplayFrame.java
+++ src/DisplayFrame.java
@@ -4,9 +4,8 @@ import java.awt.*;
 public class DisplayFrame extends JFrame {
 
     public DisplayFrame(int width, int height) {
-
-        setSize( width, height );
-        ImagePanel imagePanel = new ImagePanel(width, height);
-        add(imagePanel, BorderLayout.CENTER);
+        setSize(width, height);
+        add(new ImagePanel(width, height), BorderLayout.CENTER);
     }
+
 }

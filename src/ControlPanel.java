0fa791859e8a4efbe985a01838bd5391db27671d
thomas1242 <thomaspalmer121@gmail.com>
2017-10-25 16:11:27 -0400

    begin refactor attempt

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -3,27 +3,60 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
+import java.util.LinkedList;
 
 public class ControlPanel extends JPanel {
 
     private ImagePanel imagePanel;
-    private JButton startSearch, createMaze;
-    private JButton selectBFS, selectDFS, selectDijkstra, selectA_star;
+    private JButton startSearchButton, createMazeButton;
+    private List<JButton> algorithmSelectButtons;
+    private String selectedAlgorithm = "BFS";
     private Color textColor = new Color(0, 0, 0, 250);
-    private boolean BFS, DFS, Dijkstra, A_star;
-    private int x, y, curr_x, curr_y, width, height; 
+    private int x, y, curr_x, curr_y, width, height;
 
     public ControlPanel(ImagePanel imagePanel) {
-        
-        setLayout(new GridLayout(0, 1));
         this.imagePanel = imagePanel;
+        setupLayout();
+        addComponents();
+        addMouseListeners();
+    }
+
+    private void setupLayout() {
+        setLayout(new GridLayout(0, 1));
         width = (int)(imagePanel.getWidth() * .175);
         height = (int)(imagePanel.getHeight() * .5);
         x = curr_x = (int)(imagePanel.getWidth() * (1 - .2));
         y = curr_y = (int)(imagePanel.getHeight() * .25);
         setBounds(x, y, width, height  );
 
-        startSearch = new JButton("Start search");
+        setBackground(new Color(50, 50, 50, 200));
+        setBorder(BorderFactory.createLineBorder(new Color(220, 220, 220,  32), 6));
+        setVisible(true);
+        setOpaque(true);
+    }
+
+    private void addComponents() {
+        this.startSearchButton = createStartSearchButton();
+        this.createMazeButton = createMazeButton();
+
+        JLabel algoLabel = createAlgorithmLabel();
+        JPanel algoButtonPanel = createAlgorithmPanel();
+        JPanel clearPanel = createClearPanel();
+        JPanel speedSliderPanel = createSpeedSliderPanel();
+        JPanel sizeSliderPanel = createSizeSliderPanel();
+
+        add(startSearchButton);
+        add(algoLabel);
+        add(algoButtonPanel);
+        add(speedSliderPanel);
+        add(sizeSliderPanel);
+        add(clearPanel);
+        add(createMazeButton);
+    }
+    
+    private JButton createStartSearchButton() {
+        JButton startSearch = new JButton("Start search");
         startSearch.setForeground(  new Color(0, 175, 0, 255)  );
         startSearch.addActionListener(new ActionListener() {
             @Override
@@ -36,58 +69,104 @@ public class ControlPanel extends JPanel {
                     resumeSearch();
             }
         });
+        startSearch.setFont(new Font("plain", Font.BOLD, 13));
+        startSearch.setForeground(textColor);
+        startSearch.setOpaque(false);
 
-        createMaze = new JButton("Create Maze");
+        return startSearch;
+    }
+    
+    private JButton createMazeButton() {
+        JButton createMaze = new JButton("Create Maze");
         createMaze.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(createMaze.getText().equals("Create Maze")) 
+                if(createMaze.getText().equals("Create Maze"))
                     createMaze();
-                else if (createMaze.getText().equals( "Pause"))  
+                else if (createMaze.getText().equals( "Pause"))
                     pauseMaze();
-                else if (createMaze.getText().equals( "Resume")) 
-                   resumeMaze();
+                else if (createMaze.getText().equals( "Resume"))
+                    resumeMaze();
             }
         });
-
+        createMaze.setFont(new Font("plain", Font.BOLD, 13));
+        createMaze.setForeground(textColor);
+        createMaze.setOpaque(false);
+        
+        return createMaze;
+    }
+    
+    private JPanel createAlgorithmPanel() {
         JPanel algoPanel = new JPanel(new GridLayout(0, 2));
-        selectBFS = new JButton("BFS");
+        JButton selectBFS = new JButton("BFS");
         selectBFS.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                selectBFS();
+                defaultTextColors();
+                selectedAlgorithm = "BFS";
+                selectBFS.setForeground(  new Color(0, 175, 0, 255)  );
             }
         });
 
-        selectDFS = new JButton("DFS");
+        JButton selectDFS = new JButton("DFS");
         selectDFS.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                selectDFS();
+                defaultTextColors();
+                selectedAlgorithm = "DFS";
+                selectDFS.setForeground(  new Color(0, 175, 0, 255)  );
             }
         });
 
-        selectDijkstra = new JButton("Dijkstra");
+        JButton selectDijkstra = new JButton("Dijkstra");
         selectDijkstra.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                selectDijkstra();
+                defaultTextColors();
+                selectedAlgorithm = "Dijkstra";
+                selectDijkstra.setForeground(  new Color(0, 175, 0, 255)  );
             }
         });
 
-        selectA_star = new JButton("A*");
+        JButton selectA_star = new JButton("A*");
         selectA_star.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                selectAstar();
+                defaultTextColors();
+                selectedAlgorithm = "A*";
+                selectA_star.setForeground(  new Color(0, 175, 0, 255)  );
             }
         });
 
-        algoPanel.add(selectBFS);
-        algoPanel.add(selectDFS);
-        algoPanel.add(selectDijkstra);
-        algoPanel.add(selectA_star);
+        selectDFS.setFont(new Font("plain", Font.BOLD, 13));
+        selectBFS.setFont(new Font("plain", Font.BOLD, 13));
+        selectDijkstra.setFont(new Font("plain", Font.BOLD, 13));
+        selectA_star.setFont(new Font("plain", Font.BOLD, 13));
+        
+        selectDFS.setForeground(textColor);
+        selectBFS.setForeground(textColor);
+        selectDijkstra.setForeground(textColor);
+        selectA_star.setForeground(textColor);
+        
+        selectDFS.setOpaque(false);
+        selectBFS.setOpaque(false);
+        selectDijkstra.setOpaque(false);
+        selectA_star.setOpaque(false);
 
+        algorithmSelectButtons = new LinkedList<>();
+        algorithmSelectButtons.add(selectBFS);
+        algorithmSelectButtons.add(selectDFS);
+        algorithmSelectButtons.add(selectDijkstra);
+        algorithmSelectButtons.add(selectA_star);
+
+        for(JButton button : algorithmSelectButtons)
+            algoPanel.add(button);
+
+        algoPanel.setOpaque(false);
+        return algoPanel;
+    }
+    
+    private JPanel createClearPanel() {
         JPanel clearPanel = new JPanel(new GridLayout(0, 2));
         JButton clearObstacles, clearPath;
 
@@ -107,10 +186,19 @@ public class ControlPanel extends JPanel {
             }
         });
 
+        clearObstacles.setFont(new Font("plain", Font.BOLD, 13));
+        clearPath.setFont(new Font("plain", Font.BOLD, 13));
+        clearObstacles.setForeground(textColor);
+        clearPath.setForeground(textColor);
+        clearPanel.setOpaque(false);
+
         clearPanel.add(clearObstacles);
         clearPanel.add(clearPath);
 
-        // animation speed slider
+        return clearPanel;
+    }
+    
+    private JPanel createSpeedSliderPanel() {
         JPanel speedSlider = new JPanel();
         speedSlider.setLayout(new GridLayout(0, 1));
 
@@ -132,114 +220,106 @@ public class ControlPanel extends JPanel {
         speedSlider.add(slider, BorderLayout.SOUTH);
         speedSlider.setOpaque(false);
         speedSlider.setVisible(true);
-
-        JLabel algo_label = new JLabel(" Algorithms");
-
-        algo_label.setFont(new Font("plain", Font.BOLD, 14));
-        startSearch.setFont(new Font("plain", Font.BOLD, 13));
-        selectDFS.setFont(new Font("plain", Font.BOLD, 13));
-        selectBFS.setFont(new Font("plain", Font.BOLD, 13));
-        startSearch.setFont(new Font("plain", Font.BOLD, 13));
-        selectDijkstra.setFont(new Font("plain", Font.BOLD, 13));
-        selectA_star.setFont(new Font("plain", Font.BOLD, 13));
-        clearObstacles.setFont(new Font("plain", Font.BOLD, 13));
-        clearPath.setFont(new Font("plain", Font.BOLD, 13));
-        createMaze.setFont(new Font("plain", Font.BOLD, 13));
-
-        algo_label.setForeground( new Color(0xffbbbbbb) );
-        startSearch.setForeground(textColor);
-        clearObstacles.setForeground(textColor);
-        clearPath.setForeground(textColor);
-        createMaze.setForeground(textColor);
-        selectDFS.setForeground(textColor);
-        selectBFS.setForeground(textColor);
-        selectDijkstra.setForeground(textColor);
-        selectA_star.setForeground(textColor);
-
-        selectDFS.setOpaque(false);
-        selectBFS.setOpaque(false);
-        startSearch.setOpaque(false);
-        selectDijkstra.setOpaque(false);
-        selectA_star.setOpaque(false);
-        clearPanel.setOpaque(false);
-        algoPanel.setOpaque(false);
-
-        add(startSearch);
-        add(algo_label);
-        add(algoPanel);
-        add(speedSlider);
-        add(new SizeSlider(imagePanel));
-        add(clearPanel);
-        add(createMaze);
-
-        setBackground(new Color(50, 50, 50, 200));
-        this.setBorder(BorderFactory.createLineBorder(new Color(220, 220, 220,  32), 6));
-        setVisible(true);
-        setOpaque(true);
-
-        addMouseListener( new MouseAdapter() {
-                public void mousePressed( MouseEvent event ) {
-                    if(event.getButton() == MouseEvent.BUTTON1) {
-                        x = (int)event.getPoint().getX();
-                        y = (int)event.getPoint().getY();
-                    }
-                }
-            } );
-
-       addMouseMotionListener( new MouseMotionAdapter() {
-                public void mouseDragged(MouseEvent event) {
-                    Point p = event.getPoint();
-                    curr_x += (p.getX() - x);
-                    curr_y += (p.getY() - y);
-                    setBounds(curr_x, curr_y, width, height);
-                }
-            } );
+        
+        return speedSlider;
     }
 
-    private void clearAll() {
-        BFS = false;
-        DFS = false;
-        Dijkstra = false;
-        A_star = false;
+    private JPanel createSizeSliderPanel() {
+        JPanel sizeSliderPanel = new JPanel();
+        sizeSliderPanel.setLayout(new GridLayout(0, 1));
 
-        selectBFS.setForeground(textColor);
-        selectDFS.setForeground(textColor);
-        selectDijkstra.setForeground(textColor);
-        selectA_star.setForeground(textColor);
+        JLabel label = new JLabel();
+        label.setFont(new Font("plain", Font.BOLD, 14));
+        label.setForeground( new Color(0xffbbbbbb) );
+
+        JSlider slider = new JSlider(2, 120, 60);
+        label.setText(" " + String.valueOf(imagePanel.getHeight() / slider.getValue() + 1) + " rows , " + String.valueOf(imagePanel.getWidth() / slider.getValue() + 1)  + " columns ");
+
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int size = slider.getValue();
+                label.setText(" " + String.valueOf(imagePanel.getHeight() / size + 1) + " rows , " + String.valueOf(imagePanel.getWidth() / size + 1)  + " columns ");
+                imagePanel.updateCellSize( size );
+            }
+        });;
+
+        slider.setMinorTickSpacing(3);
+        slider.setPaintTicks(true);
+        slider.setSnapToTicks(true);
+
+        sizeSliderPanel.add(label, BorderLayout.CENTER);
+        sizeSliderPanel.add(slider, BorderLayout.SOUTH);
+
+        sizeSliderPanel.setOpaque(false );
+        sizeSliderPanel.setVisible(true);
+
+        return sizeSliderPanel;
+    }
+
+    private JLabel createAlgorithmLabel() {
+        JLabel algo_label = new JLabel(" Algorithms");
+        algo_label.setFont(new Font("plain", Font.BOLD, 14));
+        algo_label.setForeground( new Color(0xffbbbbbb) );
+        return algo_label;
+    }
+
+    private void addMouseListeners() {
+        addMouseListener( new MouseAdapter() {
+            public void mousePressed( MouseEvent event ) {
+                if(event.getButton() == MouseEvent.BUTTON1) {
+                    x = (int)event.getPoint().getX();
+                    y = (int)event.getPoint().getY();
+                }
+            }
+        } );
+        addMouseMotionListener( new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent event) {
+                Point p = event.getPoint();
+                curr_x += (p.getX() - x);
+                curr_y += (p.getY() - y);
+                setBounds(curr_x, curr_y, width, height);
+            }
+        } );
+    }
+
+    private void defaultTextColors() {
+        for(JButton button : algorithmSelectButtons)
+            button.setForeground(textColor);
     }
 
     public void setSearchText(String s, Color c) {
-        startSearch.setText(s);
-        startSearch.setForeground(c);
+        startSearchButton.setText(s);
+        startSearchButton.setForeground(c);
     }
 
     public void setMazeText(String s, Color c) {
-        createMaze.setText(s);
-        createMaze.setForeground(c);
+        createMazeButton.setText(s);
+        createMazeButton.setForeground(c);
     }
 
     public void runSearch() {
-        startSearch.setText( "Pause");
-        startSearch.setForeground(  Color.RED  );
+        startSearchButton.setText( "Pause");
+        startSearchButton.setForeground(  Color.RED  );
         imagePanel.setSearchState(true);
         doSearch();
     }
 
     public void pauseSearch() {
-        startSearch.setText( "Resume");
-        startSearch.setForeground(  new Color(0, 175, 0, 255)  );
+        startSearchButton.setText( "Resume");
+        startSearchButton.setForeground(  new Color(0, 175, 0, 255)  );
         imagePanel.setSearchState(false);
     }
 
     public void resumeSearch() {
-        startSearch.setText( "Pause");
-        startSearch.setForeground(  Color.RED  );
+        startSearchButton.setText( "Pause");
+        startSearchButton.setForeground(  Color.RED  );
         imagePanel.setSearchState(true);
     }
 
     public void createMaze() {
-        createMaze.setText( "Pause");
-        createMaze.setForeground(  Color.RED  );
+        createMazeButton.setText( "Pause");
+        createMazeButton.setForeground(  Color.RED  );
         imagePanel.setSearchState(true);
         new Thread(new Runnable() {
             @Override
@@ -250,14 +330,14 @@ public class ControlPanel extends JPanel {
     }
 
     public void pauseMaze() {
-        createMaze.setText( "Resume");
-        createMaze.setForeground(  new Color(0, 175, 0, 255)  );
+        createMazeButton.setText( "Resume");
+        createMazeButton.setForeground(  new Color(0, 175, 0, 255)  );
         imagePanel.setSearchState(false);
     }
 
     public void resumeMaze() {
-        createMaze.setText( "Pause");
-        createMaze.setForeground(  Color.RED  );
+        createMazeButton.setText( "Pause");
+        createMazeButton.setForeground(  Color.RED  );
         imagePanel.setSearchState(true);
     }
 
@@ -276,13 +356,13 @@ public class ControlPanel extends JPanel {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                if(BFS)
+                if(selectedAlgorithm.equals("BFS"))
                     imagePanel.BFS();
-                else if (DFS)
+                else if (selectedAlgorithm.equals("DFS"))
                     imagePanel.DFS();
-                else if (A_star)
+                else if (selectedAlgorithm.equals("A*"))
                     imagePanel.A_Star();
-                else if (Dijkstra)
+                else if (selectedAlgorithm.equals("Dijkstra"))
                     imagePanel.Dijkstra();
                 else
                     readySearch();
@@ -290,64 +370,4 @@ public class ControlPanel extends JPanel {
         }).start();
     }
 
-    private void selectBFS() {
-        clearAll();
-        BFS = true;
-        selectBFS.setForeground(  new Color(0, 175, 0, 255)  );
-    }
-
-    private void selectDFS() {
-        clearAll();
-        DFS = true;
-        selectDFS.setForeground(  new Color(0, 175, 0, 255)  );
-    }
-
-    private void selectAstar() {
-        clearAll();
-        A_star = true;
-        selectA_star.setForeground(  new Color(0, 175, 0, 255)  );
-    }
-
-    private void selectDijkstra() {
-        clearAll();
-        Dijkstra = true;
-        selectDijkstra.setForeground(  new Color(0, 175, 0, 255)  );
-    }
-
-}
-
-class SizeSlider extends JPanel {
-
-    private JSlider slider;
-    private JLabel label;
-
-    public SizeSlider(ImagePanel imagePanel) {
-        setLayout(new GridLayout(0, 1));
-
-        label = new JLabel();
-        label.setFont(new Font("plain", Font.BOLD, 14));
-        label.setForeground( new Color(0xffbbbbbb) );
-
-        slider = new JSlider(2, 120, 60);
-        label.setText(" " + String.valueOf(imagePanel.getHeight() / slider.getValue() + 1) + " rows , " + String.valueOf(imagePanel.getWidth() / slider.getValue() + 1)  + " columns ");
-
-        slider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                int size = slider.getValue();
-                label.setText(" " + String.valueOf(imagePanel.getHeight() / size + 1) + " rows , " + String.valueOf(imagePanel.getWidth() / size + 1)  + " columns ");
-                imagePanel.updateCellSize( size );
-            }
-        });;
-
-        slider.setMinorTickSpacing(3);
-        slider.setPaintTicks(true);
-        slider.setSnapToTicks(true);
-
-        add(label, BorderLayout.CENTER);
-        add(slider, BorderLayout.SOUTH);
-
-        setOpaque(false );
-        setVisible(true);
-    }
 }
